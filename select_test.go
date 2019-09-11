685b0936376096a52814ec039f6212db17e7f8a9
Xi Chen <xichen@uber.com>
2019-09-11 14:53:39 -0400

    Address feedback

--- select_test.go
+++ select_test.go
@@ -59,6 +59,53 @@ func TestSelectBuilderToSql(t *testing.T) {
 	assert.Equal(t, expectedArgs, args)
 }
 
+func TestSelectBuilderToSqlOnlyPreWhere(t *testing.T) {
+	subQ := Select("aa", "bb").From("dd")
+	b := Select("a", "b").
+		Prefix("WITH prefix AS ?", 0).
+		Distinct().
+		Columns("c").
+		Column("IF(d IN ("+Placeholders(3)+"), 1, 0) as stat_column", 1, 2, 3).
+		Column(Expr("a > ?", 100)).
+		Column(Alias(Eq{"b": []int{101, 102, 103}}, "b_alias")).
+		Column(Alias(subQ, "subq")).
+		From("e").
+		JoinClause("CROSS JOIN j1").
+		Join("j2").
+		LeftJoin("j3").
+		RightJoin("j4").
+		PreWhere("f = ?", 4).
+		PreWhere(Eq{"g": 5}).
+		PreWhere(map[string]interface{}{"h": 6}).
+		PreWhere(Eq{"i": []int{7, 8, 9}}).
+		PreWhere(Or{Expr("j = ?", 10), And{Eq{"k": 11}, Expr("true")}}).
+		GroupBy("l").
+		Having("m = n").
+		OrderByClause("? DESC", 1).
+		OrderBy("o ASC", "p DESC").
+		Limit(12).
+		Offset(13).
+		Suffix("FETCH FIRST ? ROWS ONLY", 14)
+
+	sql, args, err := b.ToSql()
+	assert.NoError(t, err)
+
+	expectedSql :=
+		"WITH prefix AS ? " +
+			"SELECT DISTINCT a, b, c, IF(d IN (?,?,?), 1, 0) as stat_column, a > ?, " +
+			"(b IN (?,?,?)) AS b_alias, " +
+			"(SELECT aa, bb FROM dd) AS subq " +
+			"FROM e " +
+			"CROSS JOIN j1 JOIN j2 LEFT JOIN j3 RIGHT JOIN j4 " +
+			"PREWHERE f = ? AND g = ? AND h = ? AND i IN (?,?,?) AND (j = ? OR (k = ? AND true)) " +
+			"GROUP BY l HAVING m = n ORDER BY ? DESC, o ASC, p DESC LIMIT 12 OFFSET 13 " +
+			"FETCH FIRST ? ROWS ONLY"
+	assert.Equal(t, expectedSql, sql)
+
+	expectedArgs := []interface{}{0, 1, 2, 3, 100, 101, 102, 103, 4, 5, 6, 7, 8, 9, 10, 11, 1, 14}
+	assert.Equal(t, expectedArgs, args)
+}
+
 func TestSelectBuilderFromSelect(t *testing.T) {
 	subQ := Select("c").From("d").PreWhere(Eq{"i": 0}).Where(Eq{"i": 0})
 	b := Select("a", "b").FromSelect(subQ, "subq")
